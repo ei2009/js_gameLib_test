d545cf3e853ffc0c17ce2ce7ffae98b17ec34119
hk-p114514 <hukada.gogotea@gmail.com>
2021-11-06 15:38:20 +0900

    phaser消した

---
+++ templateWebpack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+yarn add -D webpack@latest webpack-cli@latest ts-loader@latest typescript@latest webpack-cli@latest typesync@latest
+
+echo '"build": "webpack --mode=production",
+"start": "webpack serve --mode=development"'
